752f3384675348ad7a650bd4b54461df675dcb6a
Guilherme Barnes <gulhermebarnes920@gmail.com>
2025-04-08 21:44:37 +0100

    added comments for detail

--- restaurant-mgmt/src/components/Navbar.jsx
+++ restaurant-mgmt/src/components/Navbar.jsx
@@ -4,23 +4,27 @@ import { HiHome } from "react-icons/hi";
 // import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+    //used for navigating between routes i.e pages
 //   const navigate = useNavigate();
 
+//array of buttons of the navbar
+//storing them as an array and then mapping them on allows easy addition/removal of buttons wihout affecting styling
   const navItems = [
     { label: 'Orders',  },
     { label: 'Inventory',},
     { label: 'Staff MGMT' }
   ];
-
+//using tailwind css. Maybe tricky to read, but compared to regular css, is much quicker to implement
   return (
     <nav className={` sticky top-0 z-50  text-black'} `}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           
           <div className="flex-shrink-0 flex items-center">
+            {/* space for potential logo */}
             {/* <img 
               src={logo} 
-              alt="PRS Logo" 
+              alt=" Logo" 
               className="h-12 cursor-pointer hover:scale-105 transition-transform duration-300"
               onClick={() => navigate('/')}
             /> */}
@@ -35,10 +39,11 @@ export default function Navbar() {
               <HiHome className="mr-1" /> Home
             </button>
 
+                {/* mapping button array for rendering */}
             {navItems.map((item) => (
               <button
                 key={item.label}
-                // onClick={() => navigate(`/login/${item.type}`)}
+                // onClick={() => navigate(`/`)}
                 className={`px-3 py-2 rounded-xl text-sm font-medium text-blue-300 hover:bg-gray-700 `}
               >
                 {item.label} 
@@ -61,7 +66,6 @@ export default function Navbar() {
             </div>
           </div>
 
-          {/* Mobile menu button */}
     
         </div>
       </div>
