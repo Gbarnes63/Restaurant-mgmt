707224760a7f1372651162118e63fc7dd055cbb5
Guilherme Barnes <gulhermebarnes920@gmail.com>
2025-05-02 13:29:42 +0100

    Created files for services and template classess based on design documents class diagram

---
+++ restaurant-mgmt-backend/Classes/Item.py
@@ -0,0 +1,9 @@
+class Item:
+    def __init__(self, itemID, name, description, quantity):
+        self.itemID = itemID
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+
+    def updateQuantity(self):
+        pass
---
+++ restaurant-mgmt-backend/Classes/Order.py
@@ -0,0 +1,7 @@
+
+class Order:
+    def __init__(self, orderID, tableID, items, status):
+        self.orderID = orderID
+        self.tableID = tableID
+        self.items = items
+        self.status = status
